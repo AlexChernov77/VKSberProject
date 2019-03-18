85d88a7014cac1aec5145d3ead1eb9feef2a259e
Александр <achernov@aplana.com>
2019-03-18 23:08:29 +0300

    Initial Commit

---
+++ VKSberSchool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKSberSchool
+//
+//  Created by Александр on 18/03/2019.
+//  Copyright © 2019 Александр. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VKSberSchool/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  VKSberSchool
+//
+//  Created by Александр on 18/03/2019.
+//  Copyright © 2019 Александр. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
